ea0d2a40b5679211cb112fd6deb3be62768db391
Anita <ana@codewithdragos.com>
2021-04-15 12:58:07 +0200

    some changes

--- App2.js
+++ App2.js
@@ -1,21 +1,52 @@
-import React from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import "./App.css";
 
-function SecretComponent(){
-    return <h1>Secret information fro authorized users only</h1>;
-}
+//function SecretComponent(){
+// return <h1>Secret information fro authorized users only</h1>;
+//}
 
-function RegularComponent(){
-    return <h1>Everyone can see this component</h1>
-}
+//function RegularComponent(){
+//return <h1>Everyone can see this component</h1>
+//}
 
-function App2(props) {
-  return(
-      <div>
-      {props.authorized ? <SecretComponent />  : <RegularComponent />}
-      </div>
+//function App2({authorized}) {
+//return(
+// <div>
+//{authorized ? <SecretComponent />  : <RegularComponent />}
+// </div>
+//);
+//}
+
+function App() {
+  //const [emotion, setEmotion] = useState("happy");
+  // console.log(emotion);
+  // const [secondary, setSecondary] = useState("tired");
+
+  // useEffect(() => {
+  //   console.log(`Its ${emotion} around here!`);
+  //  }, [emotion]);
+
+  //  useEffect(() =>{
+  //      console.log(`Its ${secondary} around here!`);
+  //  }, [secondary]);
+
+  const [checked, toggle] = useReducer((checked) => !checked, false);
+
+  //const [checked, setChecked] = useState(false);
+  //function toggle() {
+  // setChecked((checked) => !checked);
+  // }
+
+  return (
+    <div>
+      {/* <h1>Current emotion is {emotion} and {secondary}.</h1> */}
+      {/* <button onClick={() => setEmotion("frustrated")}>Make frustrate.</button> */}
+      {/* <button onClick={() => setSecondary("crabby")}>Make crabby.</button> */}
+
+      <input type="checkbox" value={checked} onChange={toggle} />
+      <p>{checked ? "checked" : "not checked"}</p>
+    </div>
   );
 }
-
-export default App2;
-
+// onChange={() => setChecked((checked) => !checked)}
+export default App;
--- index.js
+++ index.js
@@ -2,13 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function AppTwo() {
-  return <h1>This is the Second App</h1>;
-}
+// function AppTwo() {
+// return <h1>This is the Second App</h1>;
+// }
 
-ReactDOM.render(
-    <App2 authorized={true} />,
-  document.getElementById("root")
-);
+//si pongo [] solo me dara el valor del primer elemento
+
+//if I put second and coma, he gave me the second too
+
+// const [mostImportantItem, second] = ["boots", "tents", "headlamp"];
+// console.log(mostImportantItem, second);
+
+// const [,, light] = ["boots", "tents", "headlamp"];
+// console.log(light);
+ReactDOM.render(<App2 authorized={true} />, document.getElementById("root"));
 
 // Si no quiero añadir todo el rato <div> para agregar elementos, uso React.Fragment
